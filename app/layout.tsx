db79b134583dc519e1b7508fa91ab550487564d9
Rafael Thumaza <rassraza4@gmail.com>
2023-12-02 15:21:08 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Newslatter Form",
+  description: "Code by Rafael Thumaza",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        darkSlateGrey: "hsl(234, 29%, 20%)",
+        grey: "hsl(231, 7%, 60%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;

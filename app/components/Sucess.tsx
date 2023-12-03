f4644a18b4eeeec06fc24560f2280d7f6b346ed8
Rafael Thumaza <rassraza4@gmail.com>
2023-12-03 01:49:11 -0300

    desktop style complete

--- app/components/Sucess.tsx
+++ app/components/Sucess.tsx
@@ -7,9 +7,13 @@ import sucessicon from "@/assets/images/icon-success.svg";
 export default function Sucess() {
   const useremail: string = "cocodasilva@gmail.com";
   return (
-    <main className="flex flex-col justify-between h-screen py-8 px-6">
-      <div className="flex flex-col gap-6 mt-16">
-        <Image src={sucessicon} alt="icon of sucess state" />
+    <main className="flex flex-col justify-between bg-white sm:w-[400px] sm:rounded-[30px] py-8 px-6 sm:px-14 sm:py-12">
+      <div className="flex flex-col gap-6 mt-16 sm:mt-0 sm:mb-8 ">
+        <Image
+          src={sucessicon}
+          alt="icon of sucess state"
+          className="sm:h-12 sm:-ml-2 sm:mb-2"
+        />
         <div>
           <h1>Thanks for subscribing!</h1>
           <p>
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 //media
 import mobileImage from "../assets/images/illustration-sign-up-mobile.svg";
+import desktopImage from "@/assets/images/illustration-sign-up-desktop.svg";
 
 //components
 import Info from "./components/Info";
@@ -11,14 +12,21 @@ import Sucess from "./components/Sucess";
 
 export default function Home() {
   return (
-    <main className=" bg-white">
-      <Image src={mobileImage} alt="Newslatter illustration Mobile" />
-      <div className="px-6 py-8">
-        <Info />
-        <Form />
-      </div>
+    <main className=" bg-darkSlateGrey flex items-center justify-center h-screen">
+      <Sucess />
+
       {
-        //<Sucess />
+        //<div className="bg-white sm:items-center justify-center sm:w-[800px] sm:h-[550px] sm:rounded-[30px] sm:p-4 sm:flex sm:flex-row-reverse">
+        //<Image
+        //src={desktopImage}
+        //alt="Newslatter illustration Mobile"
+        //className="sm:h-full"
+        //>
+        //<div className="px-6 py-8 sm:px-12">
+        //<Info />
+        //<Form />
+        //</div>
+        //</div>
       }
     </main>
   );
